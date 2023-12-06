63f71d987be72d85854407394e04867474bbd807
Mexion <dev@mexion.com>
2023-12-06 15:02:46 +0100

    move loginhistory to footsteps

---
+++ src/Services/Resolvers/UserResolver.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+
+namespace Yormy\LaravelFootsteps\Services\Resolvers;
+
+use Illuminate\Support\Facades\Auth;
+use Mexion\BedrockUsersv2\Domain\User\Models\Admin;
+use Mexion\BedrockUsersv2\Domain\User\Models\Member;
+
+class UserResolver
+{
+    public static function getCurrent() : Admin | Member | null
+    {
+        /**
+         * @var User $user
+         */
+        $user = Auth::user();
+
+        return $user;
+    }
+}
