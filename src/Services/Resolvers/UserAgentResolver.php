455a8f6b0f99a2c898e1c5837619c1b389b724c9
Rob <rob@email.com>
2024-04-07 16:08:03 +0200

    stan

--- src/Services/Resolvers/UserAgentResolver.php
+++ src/Services/Resolvers/UserAgentResolver.php
@@ -16,7 +16,8 @@ class UserAgentResolver
         $userAgent = $platform.' '.$agent->version($platform);
 
         $browser = $agent->browser();
-        return $userAgent . $browser.' '.$agent->version($browser);
+
+        return $userAgent.$browser.' '.$agent->version($browser);
     }
 
     public static function getFullAgent(Request $request): string
