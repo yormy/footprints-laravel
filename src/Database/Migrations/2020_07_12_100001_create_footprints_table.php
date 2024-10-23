1773028126496790bf3b0f5b31426bb641f022d6
rob <rob@prigroup.eu>
2024-10-23 10:58:26 +0200

    Allow encryption

--- src/Database/Migrations/2020_07_12_100001_create_footprints_table.php
+++ src/Database/Migrations/2020_07_12_100001_create_footprints_table.php
@@ -31,11 +31,11 @@ return new class() extends Migration
             $table->string('route')->nullable();
             $table->string('url')->nullable();
 
-            $table->json('data')->nullable();
-            $table->json('model_old')->nullable();
-            $table->json('model_changes')->nullable();
+            $table->text('data')->nullable();
+            $table->text('model_old')->nullable();
+            $table->text('model_changes')->nullable();
 
-            $table->string('ip_address')->nullable();
+            $table->text('ip_address')->nullable();
             $table->string('user_agent')->nullable();
             $table->string('browser_fingerprint', 50)->nullable();
             $table->json('location')->nullable();
